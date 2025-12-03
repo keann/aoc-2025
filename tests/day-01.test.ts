2d64ded93725a7cd961d6afa47a1e3a594159ac5
keann <code@keann.dev>
2025-12-03 15:06:42 +0700

    feat: day 1

---
+++ tests/day-01.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from 'bun:test'
+
+import exampleData from '@data/day-01/example.txt'
+import inputData from '@data/day-01/input.txt'
+
+import part1 from '@src/day-01/part-1'
+import part2 from '@src/day-01/part-2'
+
+const splitLines = (input: string) => input.split('\n').filter(Boolean)
+
+describe('Day 1', () => {
+	const example = splitLines(exampleData)
+	const input = splitLines(inputData)
+
+	const startPosition = 50
+	const size = 100
+
+	test('example 1', () => {
+		expect(part1({ rotations: example, size, startPosition })).toEqual(3)
+	})
+
+	test('input 1', () => {
+		expect(part1({ rotations: input, size, startPosition })).toEqual(1064)
+	})
+
+	test('example 2', () => {
+		expect(part2({ rotations: example, size, startPosition })).toEqual(6)
+	})
+
+	test('input 2', () => {
+		expect(part2({ rotations: input, size, startPosition })).toEqual(6122)
+	})
+})
